6760d26bf7268e2a1bcd3906b4e5b7755f0db8b8
Jonathan Birk <jona@jbirk.de>
2022-01-14 20:19:52 +0000

    Adding pieces - King

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_startup_system(create_board)
+        .add_startup_system(create_pieces)
         .run();
 }
 
@@ -55,4 +56,60 @@ fn create_board(
             });
         }
     }
+}
+
+fn create_pieces(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Load all the meshes
+    let king_handle: Handle<Mesh> =
+        asset_server.load("models/chess/kit/pieces.glb#Mesh0/Primitive0");
+    let king_cross_handle: Handle<Mesh> =
+        asset_server.load("models/chess/kit/pieces.glb#Mesh1/Primitive0");
+    let pawn_handle: Handle<Mesh> =
+        asset_server.load("models/chess/kit/pieces.glb#Mesh2/Primitive0");
+    let knight_1_handle: Handle<Mesh> =
+        asset_server.load("models/chess/kit/pieces.glb#Mesh3/Primitive0");
+    let knight_2_handle: Handle<Mesh> =
+        asset_server.load("models/chess/kit/pieces.glb#Mesh4/Primitive0");
+    let rook_handle: Handle<Mesh> =
+        asset_server.load("models/chess/kit/pieces.glb#Mesh5/Primitive0");
+    let bishop_handle: Handle<Mesh> =
+        asset_server.load("models/chess/kit/pieces.glb#Mesh6/Primitive0");
+    let queen_handle: Handle<Mesh> =
+        asset_server.load("models/chess/kit/pieces.glb#Mesh7/Primitive0");
+
+    // Add some materials
+    let white_material = materials.add(Color::rgb(1.0, 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0.0, 0.1, 0.1).into());
+
+    commands
+        .spawn_bundle(PbrBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 4.0)),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(PbrBundle {
+                mesh: king_handle.clone(),
+                material: white_material.clone(),
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0.0, -1.9));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+            parent.spawn_bundle(PbrBundle {
+                mesh: king_cross_handle.clone(),
+                material: white_material.clone(),
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0.0, -1.9));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+        });
 }
