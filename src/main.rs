151f48d340d46978641d4897439edab6bb0c9056
Jonathan Birk <jona@jbirk.de>
2022-01-14 16:39:03 +0000

    Making a game board

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,11 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_startup_system(create_board)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // Plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 8.0 })),
-        material: materials.add(Color::rgb(1.0, 0.9, 0.9).into()),
-        transform: Transform::from_translation(Vec3::new(4.0, 0.0, 4.0)),
-        ..Default::default()
-    });
-
+fn setup(mut commands: Commands) {
     // Camera
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_matrix(Mat4::from_rotation_translation(
@@ -39,4 +28,31 @@ fn setup(
         transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
         ..Default::default()
     });
+}
+
+fn create_board(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Add meshes and materials
+    let mesh = meshes.add(Mesh::from(shape::Plane { size: 1.0 }));
+    let white_material = materials.add(Color::rgb(1.0, 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0.0, 0.1, 0.1).into());
+
+    // Spawn 64 squares {
+    for i in 0..8 {
+        for j in 0..8 {
+            commands.spawn_bundle(PbrBundle {
+                mesh: mesh.clone(),
+                material: if (i + j) % 2 == 0 {
+                    black_material.clone()
+                } else {
+                    white_material.clone()
+                },
+                transform: Transform::from_translation(Vec3::new(i as f32, 0.0, j as f32)),
+                ..Default::default()
+            });
+        }
+    }
 }
