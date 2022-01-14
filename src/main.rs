39641584f8c7a4b097e1855bed2b5806ba0e8187
Jonathan Birk <jona@jbirk.de>
2022-01-14 16:18:40 +0000

    Adding a camera and a plane

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 fn main() {
     App::new()
-        .insert_resource(Msaa{ samples: 4 })
+        .insert_resource(Msaa { samples: 4 })
         .insert_resource(WindowDescriptor {
             title: "Chess!".to_string(),
             width: 1600.,
@@ -9,5 +9,34 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 8.0 })),
+        material: materials.add(Color::rgb(1.0, 0.9, 0.9).into()),
+        transform: Transform::from_translation(Vec3::new(4.0, 0.0, 4.0)),
+        ..Default::default()
+    });
+
+    // Camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_matrix(Mat4::from_rotation_translation(
+            Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+            Vec3::new(-7.0, 20.0, 4.0),
+        )),
+        ..Default::default()
+    });
+    // Light
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+        ..Default::default()
+    });
 }
